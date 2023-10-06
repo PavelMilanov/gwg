b405ebcb89ea835357f94239c80952fd2a8770a4
Pavel Milanov <milanov71@icloud.com>
2023-10-06 16:09:07 +0300

    add help menu

---
+++ text.go
@@ -0,0 +1,18 @@
+package main
+
+// цвета текста
+// const colorReset = "\033[0m"
+const RED = "\033[31m"
+const GREEN = "\033[32m"
+const YELLOW = "\033[33m"
+const BLUE = "\033[34m"
+const CYAN = "\033[36m"
+const WHITE = "\033[37m"
+
+const MENU = `Меню утилиты gwg-manager:
+gwg instal	- установка wireguard-сервера.
+gwg show	- просмотр состояния wireguard-сервера.
+gwg add		- добавления пользователя.
+gwg remove	- удаление пользователя.
+gwg stat	- просмотр подробной статистики wireguard-сервера. 
+`
