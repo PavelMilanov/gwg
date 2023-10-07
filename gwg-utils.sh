ab1acacc65c765ac8313c8d8e5fba9fc8c09c91c
pavel_milanov <milanov71@icloud.com>
2023-10-07 23:13:52 +0300

    update docs

---
+++ gwg-utils.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+set -e
+
+command=$1
+
+function installGwg {
+    echo "Installing Wireguard Server..."
+    sudo apt install -y wireguard
+
+    echo "Preparing system..."
+    sudo groupadd gwg-manager
+    sudo usermod -aG gwg-manager $USER
+    sudo chown root:gwg-manager /etc/wireguard
+    sudo chmod ug+rwx /etc/wireguard
+
+    echo "Creating template files..."
+    mv wg_template.conf /etc/wireguard/.wg_manager/ && mv client_template.conf /etc/wireguard/.wg_manager/
+
+    echo "Set gwg PATH..."
+    sudo sh -c "echo export PATH=$PATH:/usr/bin/gwg >> /etc/profile"
+    source /etc/profile
+
+    echo "Enable ipv4 forwarding..."
+    sudo sh -c "echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf"
+    sudo sysctl -p
+
+    echo "Install gwg-manager..."
+    sudo mv gwg /usr/bin
+    echo "Done"
+
+    gwg version
+
+    read -p 'You must log out to complete the installation. Ready [Y] ?' user
+    echo
+    sudo pkill -9 -u $USER
+
+}
+
+case "$command" in
+    install)
+        installGwg
+esac
